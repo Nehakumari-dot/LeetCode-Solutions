17cb6d521111479a7a9c10171f6777a69a86c395
Nehakumari-dot <gayaneha052@gmail.com>
2026-06-09 17:17:38 +0530

    Added solution - LeetHub

--- 701716-first-repeating-element-GFG/701716-first-repeating-element.cpp
+++ 701716-first-repeating-element-GFG/701716-first-repeating-element.cpp
@@ -1,17 +1,25 @@
 class Solution {
   public:
     int firstRepeated(vector<int> &arr) {
-        int n=arr.size();
-        int f=1000001;
-        int freq[f]={0};
-        for(int i=0;i<n;i++){
-            freq[arr[i]]++;
-        }
-        for(int i=0;i<n;i++){
-            if(freq[arr[i]]>1)
-             return i+1;
-        }
-        return -1;
+       int n=arr.size();
+       int answer;
+       for(int i=0,check=0;i<n;i++){
+           if(arr[i]==arr[check]&&i!=check){
+               answer=check+1;
+               return answer;
+           }
+           
+           if(i==n-1&&i!=0){
+               check++;
+               i=check;
+           }
+           
+           if(check==n-1){
+               answer=-1;
+               return answer;
+           }
+           
+       }
          
         
             
